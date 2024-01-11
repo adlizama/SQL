264dfef3cfeb0604686c82c5a6fd539450334240
Anthony Lizama <anthony.lizama@kellogg.northwestern.edu>
2024-01-11 03:21:27 +0000

    Lab Notes

--- demo.sql
+++ demo.sql
@@ -1,3 +1,18 @@
 -- Get the first and last name of every player in the database.
 
 SELECT first_name, last_name FROM players;
+-- sqlite3 baseball.sqlite3
+-- .schema
+-- .mode columns
+-- .headers on
+-- .read
+--.quit
+
+-- count(*), AVG(column), SUM(column)
+
+--SELECT_____ FROM_____
+--WHERE______
+--AND ______
+--GROUP BY______
+--ORDER BY_____
+--LIMIT____
